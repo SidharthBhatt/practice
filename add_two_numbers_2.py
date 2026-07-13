fa1cd43d5542c55826c9f137f6e14f14246b0137
Sidharth Bhatt <81537231+SidharthBhatt@users.noreply.github.com>
2026-07-12 23:36:10 -0400

    Add function to sum two linked lists
    Implement a function to add two numbers represented by linked lists.

---
+++ add_two_numbers_2.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, x1, y1): 
+        while (x1.next is not None and y1.next is not None):
+            val = x1.val + y1.val 
+            print(z)
+            z = ListNode(val, None)
+            try ()
+            if (previous is not None)
+            x1 = x1.next
+            y1 = y1.next
+
+        
+        

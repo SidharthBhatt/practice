c51cd2b0ac8f45cb49b27d860031f4ebf2c74cfc
Sidharth Bhatt <81537231+SidharthBhatt@users.noreply.github.com>
2026-08-10 22:19:20 -0700

    Time: 28 ms (5.82%) | Memory: 19.4 MB (18.96%) - LeetSync

---
+++ LinkedList/Easy/9-palindrome-number/palindrome-number.py
@@ -0,0 +1,32 @@
+import math 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        # string method
+        # temp = str(x)
+        # reverse = temp[::-1]
+        # return bool(temp == reverse)
+        
+        # pure math method 
+        if x < 0:
+            return False
+        if x < 10:
+            return True; 
+        temp = x
+        reverse = 0
+        while (temp > 0):
+            reverse = reverse*10 + temp%10 
+            temp = math.floor(temp/10)
+        print(x)
+        print(reverse)
+        return bool(x == int(reverse))
+
+        # also attempted hashmap
+        # if x < 0:
+        #     return false 
+        # dict = {}
+        # last_half = x
+        # for i in range(math.log(x,10)/2):
+        #         dict.update({i: last_half%10})
+        #         last_half/10
+        

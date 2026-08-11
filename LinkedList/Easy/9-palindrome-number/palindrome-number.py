4e9eda1e286d6edca1cfe1ac4fd156af08d4fc2b
Sidharth Bhatt <81537231+SidharthBhatt@users.noreply.github.com>
2026-08-10 23:05:26 -0700

    Time: 2 ms (91.23%) | Memory: 19.7 MB (18.96%) - LeetSync

--- LinkedList/Easy/9-palindrome-number/palindrome-number.py
+++ LinkedList/Easy/9-palindrome-number/palindrome-number.py
@@ -18,18 +18,9 @@ class Solution:
         for i in range(math.floor(length/2)):
             reverse = reverse*10 + temp%10 
             temp = math.floor(temp/10)
-        if (length % 2 == 0):
+        if (length % 2 == 0): 
             return bool(reverse -  temp == 0)
-        temp = math.floor(temp/10)
+        temp = math.floor(temp/10) # remove middle digit if the # of digits is odd
         return bool(reverse -  temp == 0)
     
-
-        # also attempted hashmap
-        # if x < 0:
-        #     return false 
-        # dict = {}
-        # last_half = x
-        # for i in range(math.log(x,10)/2):
-        #         dict.update({i: last_half%10})
-        #         last_half/10
         
